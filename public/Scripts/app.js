b6c92a6d4ad3aaca724aff84b25586b97dd6f3f5
Caleb Okafor <cokafo11@my.centennialcollege.ca>
2023-02-12 21:57:44 -0500

    first commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,10 @@
+/*Name: Caleb Okafor (301151683)
+  Course: COMP229 Assignment 01
+  Date: Feb 12, 2022.
+  Description: JavaScript file to test the application.*/
+(function() {
+    function start() {
+        console.log("App started...");
+    }
+    window.addEventListener("load", start);
+})();
